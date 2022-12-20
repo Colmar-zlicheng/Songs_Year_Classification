35b327bc351187498a53fbfeaca28c63b8e4711e
colmar <zlicheng@sjtu.edu.cn>
2022-12-21 02:27:30 +0800

    init

--- train_ANN_songs.py
+++ train_ANN_songs.py
@@ -1,1 +1,24 @@
 import torch
+import argparse
+from lib.utils.etqdm import etqdm
+from lib.dataset.Songs import Songs
+from lib.utils.misc import bar_perfixes
+from torch.utils.tensorboard import SummaryWriter
+from lib.utils.logger import logger
+from lib.model.Songs_Years import Songs_Years
+
+
+def ANN_worker():
+
+    model = Songs_Years()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--batch_size', type=int, default=100)
+
+    arg = parser.parse_args()
+
+    logger.set_log_file('exp/ANN/dev', 'dev')
+    Songs()
+    ANN_worker()
