9e5825ffc12074a67162b64960107b5f1c9ebcc7
colmar <zlicheng@sjtu.edu.cn>
2022-12-21 16:24:11 +0800

    Model

--- lib/model/Songs_Years.py
+++ lib/model/Songs_Years.py
@@ -1,13 +1,37 @@
 import torch
 import torch.nn as nn
+from lib.utils.logger import logger
+from lib.utils.misc import param_size
 
 
 class Songs_Years(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_years=42):
         super().__init__()
-        self.layer = nn.Linear(128, 128)
+        self.name = type(self).__name__
+        self.bn_avg = nn.BatchNorm1d(12)
+        self.bn_cov = nn.BatchNorm1d(78)
+        self.avg_encoder = nn.Sequential(nn.Linear(12, 128), nn.ReLU(), nn.Linear(128, 12))
+        self.cov_encoder = nn.Sequential(nn.Linear(78, 128), nn.ReLU(), nn.Linear(128, 78))
+        self.avg_encoder_layer = nn.Sequential(nn.Linear(12, 128), nn.ReLU(), nn.Linear(128, 128))
+        self.cov_encoder_layer = nn.Sequential(nn.Linear(78, 128), nn.ReLU(), nn.Linear(128, 128))
+        self.classification_layer = nn.Sequential(nn.Linear(256, 128), nn.ReLU(), nn.Linear(128, num_years))
+        self.compute_loss = nn.CrossEntropyLoss()
+        logger.info(f"{self.name} has {param_size(self)}M parameters")
 
     def forward(self, input):
-        x = self.layer(input)
-        return x
+        avg = input['timbre_avg']
+        cov = input['timbre_cov']
+
+        avg_coder = self.bn_avg(self.avg_encoder(avg) + avg)  # [B, 12]
+        cov_coder = self.bn_cov(self.cov_encoder(cov) + cov)  # [B, 78]
+
+        avg_coder = self.avg_encoder_layer(avg_coder)  # [B, 128]
+        cov_coder = self.cov_encoder_layer(cov_coder)  # [B, 128]
+
+        feature = torch.cat([avg_coder, cov_coder], dim=1)  # [B, 256]
+        pred = self.classification_layer(feature)
+
+        year = input['year'] - 1969
+        loss = self.compute_loss(pred, year)
+        return pred, loss
