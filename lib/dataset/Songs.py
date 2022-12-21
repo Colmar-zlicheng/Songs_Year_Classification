5ea48f4dd385aa1496d06263dbbd664f0b38ef1a
colmar <zlicheng@sjtu.edu.cn>
2022-12-22 01:59:55 +0800

    Songs

--- lib/dataset/Songs.py
+++ lib/dataset/Songs.py
@@ -117,3 +117,75 @@ class Songs(torch.utils.data.Dataset):
             sample['timbre_cov'] = self.data['timbre_cov_test'][item].to(self.device)
             sample['year'] = self.data['year_test'][item].to(self.device)
         return sample
+
+
+class Songs_Total(torch.utils.data.Dataset):
+
+    def __init__(self, data_split='train', device='cpu', seed=0):
+        super().__init__()
+        self.root = './data'
+        self.path = './data/songs.csv'
+        self.data_split = data_split
+        self.device = device
+        self.data = self.Songs_whole(seed=seed)
+
+    def Songs_whole(self, train_size=2000, test_size=200, seed=0):
+        # 1969-2010: train_size + test_size for each year
+        # train_size + test_size should less than 2200
+        cache_name = f"data_dict_total.pkl"
+        cache_path = os.path.join(self.root, cache_name)
+        torch.manual_seed(seed)
+        if not os.path.exists(cache_path):
+            logger.info("Cache not exist, beginning generate and save data into {}".format(cache_path))
+            data_raw = pd.read_csv(self.path)
+            year = data_raw['year']
+            year = torch.tensor(year)
+
+            timbre_avg = []
+            timbre_cov = []
+
+            for i_avg in etqdm(range(12)):
+                timbre_avg_i = f"timbre_avg_{i_avg}"
+                timbre_avg_y = torch.tensor(data_raw[timbre_avg_i], dtype=torch.float32)
+                timbre_avg.append(timbre_avg_y)
+            timbre_avg = torch.stack(timbre_avg, dim=0).transpose(0, 1)
+
+            for i_cov in etqdm(range(78)):
+                timbre_cov_i = f"timbre_cov_{i_cov}"
+                timbre_cov_y = torch.tensor(data_raw[timbre_cov_i], dtype=torch.float32)
+                timbre_cov.append(timbre_cov_y)
+            timbre_cov = torch.stack(timbre_cov, dim=0).transpose(0, 1)
+            # 463715, 51630
+            data_whole = dict()
+            data_whole['timbre_avg_train'] = timbre_avg[:463715]
+            data_whole['timbre_avg_test'] = timbre_avg[463715:]
+            data_whole['timbre_cov_train'] = timbre_cov[:463715]
+            data_whole['timbre_cov_test'] = timbre_cov[463715:]
+            data_whole['year_train'] = year[:463715]
+            data_whole['year_test'] = year[463715:]
+            with open(cache_path, 'wb') as f:
+                pickle.dump(data_whole, f)
+        else:
+            logger.info("Load Data from cache:{}".format(cache_path))
+            with open(cache_path, 'rb') as ff:
+                data_whole = pickle.load(ff)
+        return data_whole
+
+    def __len__(self):
+        if self.data_split == 'train':
+            self.len = len(self.data['year_train'])
+        else:
+            self.len = len(self.data['year_test'])
+        return self.len
+
+    def __getitem__(self, item):
+        sample = dict()
+        if self.data_split == 'train':
+            sample['timbre_avg'] = self.data['timbre_avg_train'][item].to(self.device)
+            sample['timbre_cov'] = self.data['timbre_cov_train'][item].to(self.device)
+            sample['year'] = self.data['year_train'][item].to(self.device)
+        else:
+            sample['timbre_avg'] = self.data['timbre_avg_test'][item].to(self.device)
+            sample['timbre_cov'] = self.data['timbre_cov_test'][item].to(self.device)
+            sample['year'] = self.data['year_test'][item].to(self.device)
+        return sample
