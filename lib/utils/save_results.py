7a7f3bb03666ea668c60f1d26d66934a24bce7bf
colmar <zlicheng@sjtu.edu.cn>
2022-12-22 01:59:57 +0800

    Songs

--- lib/utils/save_results.py
+++ lib/utils/save_results.py
@@ -11,7 +11,9 @@ def save_results_ANN(arg, train_acc, test_acc):
 
     total = len(open(log_path).readlines())
     if total == 0:
-        csv_writer.writerow(['ID', 'Epoch_size', 'Batch_size', 'learning_rate',
+        csv_writer.writerow(['ID', 'Model',
+                             'Dataset', 'Train_Size', 'Test_Size',
+                             'Epoch_size', 'Batch_size', 'learning_rate',
                              'decay_step', 'decay_gamma',
                              'train_acc', 'test_acc', 'exp'])
         total += 1
@@ -19,8 +21,15 @@ def save_results_ANN(arg, train_acc, test_acc):
         decay_step = '--'
     else:
         decay_step = arg.decay_step
+    if arg.big_dataset is True:
+        dataset_name = 'Big'
+    else:
+        dataset_name = 'Small'
+
     exp = os.path.join('./exp/ANN', arg.exp_id)
-    csv_writer.writerow([str(total), str(arg.epoch_size), str(arg.batch_size), str(arg.learning_rate),
+    csv_writer.writerow([str(total), str(arg.model),
+                         str(dataset_name), str(arg.train_size), str(arg.test_size),
+                         str(arg.epoch_size), str(arg.batch_size), str(arg.learning_rate),
                          str(decay_step), str(arg.decay_gamma),
                          str(train_acc)+'%', str(test_acc)+'%', str(exp)])
     file.close()
